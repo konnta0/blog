d444725f9a6daed339aac46ad8d9f62649dd49c5
konnta0 <konnta0@gmail.com>
2023-12-14 00:08:26 +0900

    wip

--- src/Blog/Program.cs
+++ src/Blog/Program.cs
@@ -1,3 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿
+await Bootstrapper
+        .Factory
+        .CreateWeb(args)
+        .RunAsync();
